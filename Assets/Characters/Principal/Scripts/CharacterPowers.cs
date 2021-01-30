1c5389854ca791891820f9dfd0096e114e6f5188
Dani <as>
2021-01-30 20:58:36 +0000

    z

--- Assets/Characters/Principal/Scripts/CharacterPowers.cs
+++ Assets/Characters/Principal/Scripts/CharacterPowers.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class CharacterPowers : MonoBehaviour
 {
     //Prefab references
-    public GameObject personalityPrefat;
+    public GameObject personalitySensitivePrefat;
+    public GameObject personalityBravePrefat;
+
 
     // Spawn Points
     public Transform wherePersonalitiesDrop;
 
-    private bool hasPersonality = false;
+    private bool hasPersonalitySensitive = false;
+    private bool hasPersonalityBrave = false;
+
+    //private List<GameObject> personalities = new List<GameObject>();
 
     Collider2D nearWall;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -27,11 +31,11 @@ public class CharacterPowers : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             // check is we have sensitive personality and are near wall
-            if (nearWall && hasPersonality)
+            if (nearWall && hasPersonalitySensitive)
             {
                nearWall.isTrigger = true;
-               Instantiate(personalityPrefat, wherePersonalitiesDrop.position, Quaternion.identity);
-               hasPersonality = false;
+               Instantiate(personalitySensitivePrefat, wherePersonalitiesDrop.position, Quaternion.identity);
+               hasPersonalitySensitive = false;
             }
         }
 
@@ -44,13 +48,14 @@ public class CharacterPowers : MonoBehaviour
             return;
         }
         if (collision.CompareTag("Personality")){
-            hasPersonality = true;
+            hasPersonalitySensitive = true;
             Destroy(collision.gameObject);
+
             return;
         }
         if (collision.CompareTag("EndLevelDoor"))
         {
-            if(hasPersonality)
+            if(hasPersonalitySensitive)
                 collision.GetComponent<DoorController>().Open();
             else
                 collision.GetComponent<DoorController>().Close();
