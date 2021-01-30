6ef9e603a763b9658e2fe76eae984a2c2665fb34
Dani <as>
2021-01-30 23:45:28 +0000

    lamapdas

---
+++ Assets/Characters/CanvasPersonalities.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasPersonalities : MonoBehaviour
+{
+    public GameObject sensitiveImg;
+    public GameObject braveImg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    public void setSensitiveActive(bool active)
+    {
+        sensitiveImg.SetActive(active);
+    }
+    public void setBraveActive(bool active)
+    {
+        braveImg.SetActive(active);
+    }
+}
--- Assets/Characters/Principal/Scripts/CharacterPowers.cs
+++ Assets/Characters/Principal/Scripts/CharacterPowers.cs
@@ -21,11 +21,25 @@ public class CharacterPowers : MonoBehaviour
     private Rigidbody2D rb2D;
     private float thrust = 10.0f;
 
+    public bool HasPersonalitySensitive { get => hasPersonalitySensitive; set => hasPersonalitySensitive = value; }
+    public bool HasPersonalityBrave { get => hasPersonalityBrave; set => hasPersonalityBrave = value; }
+
+    public GameObject canvasObject;
+    private CanvasPersonalities canvas;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
-
+        
+        foreach (Canvas c in FindObjectsOfType<Canvas>())
+        {
+            if (c.CompareTag("MainCamera"))
+            {
+                canvas = c.GetComponent<CanvasPersonalities>();
+                break;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -35,21 +49,24 @@ public class CharacterPowers : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             // check is we have sensitive personality and are near wall
-            if (nearWall && hasPersonalitySensitive)
+            if (nearWall && HasPersonalitySensitive)
             {
                nearWall.isTrigger = true;
                Instantiate(personalitySensitivePrefat, wherePersonalitiesDrop.position, Quaternion.identity);
-               hasPersonalitySensitive = false;
+               HasPersonalitySensitive = false;
+                canvas.setSensitiveActive(false);
             }
         }
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             // check is we have sensitive personality and are near wall
-            if (hasPersonalityBrave)
+            if (HasPersonalityBrave)
             {
                 rb2D.AddForce(transform.up * thrust, ForceMode2D.Impulse);
                 Instantiate(personalityBravePrefat, wherePersonalitiesDrop.position, Quaternion.identity);
-                hasPersonalityBrave = false;
+                HasPersonalityBrave = false;
+                canvas.setBraveActive(false);
+
             }
         }
 
@@ -62,22 +79,25 @@ public class CharacterPowers : MonoBehaviour
             return;
         }
         if (collision.CompareTag("SensitivePersonality")){
-            hasPersonalitySensitive = true;
+            HasPersonalitySensitive = true;
+            canvas.setSensitiveActive(true);
+
             Destroy(collision.gameObject);
 
             return;
         }
         if (collision.CompareTag("BravePersonality"))
         {
-            hasPersonalityBrave = true;
+            HasPersonalityBrave = true;
+            canvas.setBraveActive(true);
             Destroy(collision.gameObject);
 
             return;
         }
         if (collision.CompareTag("EndLevelDoor"))
         {
-            Debug.Log(hasPersonalitySensitive);
-            if (hasPersonalitySensitive) { 
+            Debug.Log(HasPersonalitySensitive);
+            if (HasPersonalitySensitive) { 
                 Debug.Log("abertop");
                 collision.GetComponent<DoorController>().Open();
             }
