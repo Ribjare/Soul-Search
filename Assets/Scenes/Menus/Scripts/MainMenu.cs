247e5b98039d704a3f5137cd07f3644516d60682
Sarabatista16t <46160145+Sarabatista16t@users.noreply.github.com>
2021-01-31 12:22:28 +0000

    Main menu, pause menu and controls menu

---
+++ Assets/Scenes/Menus/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void PlayGame()
+    {
+        Cursor.visible = false;
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void ShowControls()
+    {
+        Cursor.visible = false;
+
+        //Show controls menu
+        SceneManager.LoadScene("ControlsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scenes/Menus/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false); // Hide the Pause menu
+        Time.timeScale = 1f; // Restart the time in the game
+        GameIsPaused = false;
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true); // Show the Pause menu
+        Time.timeScale = 0f; // Stop the time in the game
+        GameIsPaused = true;
+        //Set Cursor to be visible
+        Cursor.visible = true;
+    }
+
+    public void LoadMenu()
+    {
+        ResumeGame();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
