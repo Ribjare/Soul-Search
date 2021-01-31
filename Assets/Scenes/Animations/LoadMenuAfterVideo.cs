31c678eccdbf4be79553ebb960c2583980689d29
Sarabatista16t <46160145+Sarabatista16t@users.noreply.github.com>
2021-01-31 16:06:06 +0000

    Final commit

---
+++ Assets/Scenes/Animations/LoadMenuAfterVideo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class LoadMenuAfterVideo : MonoBehaviour
+{
+    public VideoPlayer VideoPlayer; // Drag & Drop the GameObject holding the VideoPlayer component
+
+    void Start()
+    {
+        VideoPlayer.loopPointReached += LoadScene;
+    }
+
+    void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("MainMenu");
+        Cursor.visible = true;
+    }
+}
